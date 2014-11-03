f286ac1706990b66815c95271cc54172234e0295
gjkf <cossu.dvd@gmail.com>
2014-11-03 19:08:54 +0100

    I got an idea, write the expression and then calcolate it. The keyboard will only show up if necessary.

--- Calculator/src/com/gjkf/calc/gui/MainView.java
+++ Calculator/src/com/gjkf/calc/gui/MainView.java
@@ -17,6 +17,8 @@
 package com.gjkf.calc.gui;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
@@ -25,16 +27,20 @@ public class MainView{
 
 	private Display display;
 	private static Shell shell;
-	
-	private Button[] numberButtons = new Button[10];//btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
-	
+
+	private Button[] numberButtons = new Button[10];
+	private Button plusButton, minusButton, timesButton, divideButton;
+
+	/**
+	 * @wbp.parser.entryPoint
+	 */
 	public void init(Display d){
-		
+
 		this.display = d;
-		
+
 		shell = new Shell(display, SWT.SHELL_TRIM);
-		shell.setSize(600, 600);
-		shell.setMinimumSize(600, 600);
+		shell.setMinimumSize(600, 200);
+		shell.setSize(1000, 600);
 		shell.setLayout(null);
 		shell.setText("Scientific Calculator");
 
@@ -42,20 +48,34 @@ public class MainView{
 		center();
 		open();
 	}
-	
+
 	public void initButtons(){
-		
+
 		/*
 		 * Better way of handling 10 different digits
 		 */
-		
+
 		for(int i = 0; i < numberButtons.length; i++){
 			numberButtons[i] = new Button(shell, SWT.PUSH);
-			numberButtons[i].setBounds(25 + 55*i, 200, 50, 50);
+			numberButtons[i].setBounds(25 + 55*i, 250, 50, 50);
 			numberButtons[i].setText(Integer.toString(i));
 		}
-		
+
+		plusButton = new Button(shell, SWT.PUSH);
+		minusButton = new Button(shell, SWT.PUSH);
+		timesButton = new Button(shell, SWT.PUSH);
+		divideButton = new Button(shell, SWT.PUSH);
+
+		setOperationButtons(plusButton, "+", 25, 320, 45, 45);
+		setOperationButtons(minusButton, "-", 80, 320, 45, 45);
+		setOperationButtons(timesButton, "x", 135, 320, 45, 45);
+		setOperationButtons(divideButton, "/", 190, 320, 45, 45);
+
 	}
+
+	/*
+	 * This ensures that the shell stays open until it's closed
+	 */
 
 	public void open(){
 		shell.open();
@@ -64,6 +84,37 @@ public class MainView{
 				display.sleep();
 			}
 		}
+	}
+
+	public void setOperationButtons(Button btn, String text, int x, int y, int width, int height){
+		if(btn != null){
+			btn.setBounds(x, y, width, height);
+			btn.setText(text);
+		}
+	}
+
+	public void setListeners(Button btn, String ident){
+
+		if(ident.equals("+")){
+			plusButton.addMouseListener(new MouseAdapter(){
+
+				@Override
+				public void mouseDown(MouseEvent e){
+					System.out.println("+");
+				}
+
+			});
+		}else if(ident.equals("-")){
+			minusButton.addMouseListener(new MouseAdapter(){
+				
+				@Override
+				public void mouseDown(MouseEvent e){
+					System.out.println("-");
+				}
+				
+			});
+		}
+
 	}
 
 	/*
@@ -77,5 +128,5 @@ public class MainView{
 		int nTop = (bds.height - p.y) / 2;
 		shell.setBounds(nLeft, nTop, p.x, p.y);
 	}
-	
+
 }
