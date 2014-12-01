a55828b019f7f1f5ca71ecabb744a61ba38f8526
gjkf <cossu.dvd@gmail.com>
2014-12-01 19:42:20 +0100

    First commit with IntelliJ Idea

---
+++ src/com/gjkf/calc/core/Core.java
@@ -0,0 +1,228 @@
+/*
+ * Copyright (c) 2014 Davide Cossu.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses>.
+ */
+
+package com.gjkf.calc.core;
+
+import com.gjkf.calc.gui.MainView;
+
+public class Core{
+
+	public static double x = 0, y = 0, z = 0, t = 0;
+
+	public static void clearStack(){
+
+		x = 0; y = 0; z = 0; t = 0;
+
+	}
+
+	/**
+	 *
+	 * @param formula
+	 * @param multiplier
+	 * @param cycle
+	 */
+
+	public static void draw(String formula, double multiplier, int cycle){
+
+		String[] args;
+
+		double yValue = 0., tempX = 0., tempY = 0., currX, oldX = 0., oldY = 0.;
+
+		String oper = "";
+
+		// This stores wether or not I've already put a value in the 'x'
+		boolean flag = false;
+		
+		int var = (int) (-100 * multiplier);
+
+		/*
+		 * Checks if the given formula is not null
+		 */
+		
+		if(formula != null){
+
+			/*
+			 * Puts the split String inside an array. It splits the formula at each space
+			 */
+			
+			args = formula.split(" ");
+
+			for(currX = var; currX < -var; currX += 1){
+
+				for(int i = 0; i < args.length; i++){
+
+					/*
+					 * If the element at the 'i' position is an 'x', otherwise I check if it's an operator 
+					 */
+					
+					//System.out.println("Args[i]: " + args[i] + " " + i);
+					
+					if(args[i].equalsIgnoreCase("x")){
+
+						if(flag)
+							tempX = currX;
+						else
+							tempY = currX;
+						
+					}else{
+
+						if(args[i].equals("*") || args[i].equals("/") || args[i].equals("+") || args[i].equals("-") || args[i].equals("sin") || args[i].equals("cos")){
+
+							//System.out.println("ArgsOperation[i]: " + args[i] + " " + i);
+
+							// I put the element inside the "oper" variable
+							oper = args[i];
+
+							flag = true;
+
+							//System.out.println("Flag: " + flag);
+
+							continue;
+
+						}
+
+						//System.out.println("Flag: " + flag);
+
+						/*
+						 * If I already have a value stored, I put in the "tempX" varable the current element of the array, otherwise I put it in the 'Y' value
+						 */
+						
+						if(flag)
+							tempX = Double.parseDouble(args[i]);
+						else
+							tempY = Double.parseDouble(args[i]);
+
+					}
+
+					//System.out.println("TempX: " + tempX);
+					//System.out.println("TempY: " + tempY);
+
+					/*
+					 * If I have a value, I must have another one ready to be calculated, so I check what operator I've stored before and do the proper calculation
+					 */
+					
+					if(flag){
+
+						flag  = false;
+
+						if(oper.equals("+")){
+							
+							yValue = tempX + tempY;
+
+							tempY = yValue;
+
+							System.out.println("X: " + currX);
+							System.out.println("Y: " + yValue + "\n");
+							
+						}
+						
+						if(oper.equals("*")){
+							
+							yValue = tempX * tempY;
+
+							tempY = yValue;
+
+							System.out.println("X: " + currX);
+							System.out.println("Y: " + yValue + "\n");
+							
+						}
+
+						if(oper.equals("-")){
+							
+							yValue = tempY - tempX;
+
+							tempY = yValue;
+
+							System.out.println("X: " + currX);
+							System.out.println("Y: " + yValue + "\n");
+							
+						}
+
+						if(oper.equals("/")){
+
+							if(tempX != 0){
+
+								yValue = tempY / tempX;
+
+								tempY = yValue;
+
+								System.out.println("X: " + currX);
+								System.out.println("Y: " + yValue + "\n");
+
+							}else{
+
+								System.err.println("Exception Occurred: Can't divide by '0'");
+
+							}
+
+						}
+						
+						if(oper.equals("sin")){
+							
+							yValue = Math.sin((tempX / 180) * Math.PI);
+							
+							tempY = yValue;
+							
+							//System.out.println("sin(45): " + Math.sin(Math.toDegrees(45)));
+							
+							System.out.println("X: " + currX);
+							System.out.println("Y: " + yValue + "\n");
+							
+						}
+						
+						if(oper.equals("cos")){
+							
+							yValue = Math.cos((tempX / 180) * Math.PI);
+
+							tempY = yValue;
+							
+							System.out.println("X: " + currX);
+							System.out.println("Y: " + yValue + "\n");
+							
+						}
+
+						/*
+						 * If the currX (incremented in the for loop) is not equals to the var, so it's not the first time, I draw the point
+						 */
+						
+						if(currX != var)
+							MainView.draw(400 + (int) oldX * (int) multiplier, 185 - (int) oldY * (int) multiplier, 400 + (int) currX * (int) multiplier, 185 - (int) yValue * (int) multiplier, cycle);
+						
+						/*
+						 * I make sure to store the "old" 'x' and 'y' values in other values for later use (see the draw line above) 
+						 */
+						
+						oldX = currX;
+						oldY = yValue;
+
+					}
+
+				}
+
+			}
+
+			if(!MainView.isChanged())
+				MainView.augmentCicle();
+			
+			//System.out.println("Changed: " + MainView.isChanged() + "\n");
+			//System.err.println("Cicle: " + cicle + "\n");
+			//System.err.println("MainViewCicle: " + MainView.drawCicle + "\n");
+			
+		}
+		
+	}
+
+}
---
+++ src/com/gjkf/calc/gui/MainView.java
@@ -0,0 +1,345 @@
+/*
+ * Copyright (c) 2014 Davide Cossu.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses>.
+ */
+
+package com.gjkf.calc.gui;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CLabel;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.KeyListener;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+
+import com.gjkf.calc.core.Core;
+
+public class MainView{
+
+	private static Display display;
+	private static Shell shell;
+
+	private Button extendButton;
+
+	private KeyBoard keyBoard;
+
+	private static CLabel expressionLabel, xStackLabel, yStackLabel, zStackLabel, tStackLabel, yLabel, multLabel;
+
+	private Text formulaField, multiplierTextField;
+
+	private static GC gc;
+
+	private static boolean extended = false, changed = false;
+
+	// Time in ms
+	private int time = 10;
+	public static int drawCycle;
+
+	private String formula = ""; 
+
+	@SuppressWarnings("static-access")
+	public void init(Display d){
+
+		this.display = d;
+
+		shell = new Shell(display, SWT.SHELL_TRIM);
+		shell.setMinimumSize(1000, 600);
+		shell.setSize(1000, 600);
+		shell.setLayout(null);
+		shell.setText("Scientific Calculator");
+
+		initButtons();
+		initLabels();
+		initTextField();
+
+		initTimer();
+
+		center();
+		open();
+
+	}
+
+	public static void draw(int x1, int y1, int x2, int y2, int cycle){
+
+		gc = new GC(shell);
+		
+		System.out.println("Draw:cycle: " + cycle);
+		
+		if(cycle <= 0)
+			gc.setForeground(display.getSystemColor(SWT.COLOR_BLACK));
+		else if(cycle == 1)
+			gc.setForeground(display.getSystemColor(SWT.COLOR_RED));
+		else if(cycle == 2)
+			gc.setForeground(display.getSystemColor(SWT.COLOR_BLUE));
+		else if(cycle == 3)
+			gc.setForeground(display.getSystemColor(SWT.COLOR_GREEN));
+		else if(cycle == 4)
+			gc.setForeground(display.getSystemColor(SWT.COLOR_CYAN));
+		else if(cycle == 5)
+			gc.setForeground(display.getSystemColor(SWT.COLOR_MAGENTA));
+		else if(cycle == 6)
+			gc.setForeground(display.getSystemColor(SWT.COLOR_YELLOW));
+		else if(cycle == 7)
+			gc.setForeground(display.getSystemColor(SWT.COLOR_WHITE));
+		
+		gc.drawLine(x1, y1, x2, y2);
+
+		gc.dispose();
+
+	}
+
+	private void initLabels(){
+
+		expressionLabel = new CLabel(shell, SWT.SHADOW_IN);
+		expressionLabel.setBounds(300, 300, 650, 100);
+
+		initStackLabels();
+
+	}
+
+	private void initStackLabels(){
+
+		if(extended){
+			xStackLabel = new CLabel(shell, SWT.SHADOW_NONE);
+			xStackLabel.setBounds(25, 170, 250, 30);
+			yStackLabel = new CLabel(shell, SWT.SHADOW_NONE);
+			yStackLabel.setBounds(25, 200, 250, 30);
+			zStackLabel = new CLabel(shell, SWT.SHADOW_NONE);
+			zStackLabel.setBounds(25, 230, 250, 30);
+			tStackLabel = new CLabel(shell, SWT.SHADOW_NONE);
+			tStackLabel.setBounds(25, 260, 250, 30);
+		}
+
+	}
+
+	private void initTextField(){
+
+		multLabel = new CLabel(shell, SWT.SHADOW_NONE);
+		multLabel.setBounds(55, 490, 100, 40);
+		multLabel.setText("Multiplier = ");
+		
+		multiplierTextField = new Text(shell, SWT.CENTER);
+		multiplierTextField.setBounds(150, 500, 70, 20);
+		multiplierTextField.setMessage("Multiplier");
+		
+		multiplierTextField.addKeyListener(new KeyListener() {
+
+			@Override
+			public void keyReleased(KeyEvent e){}
+
+			@Override
+			public void keyPressed(KeyEvent e){
+
+				if(e.keyCode == 13){
+
+					System.out.println(formula);
+
+					if(!multiplierTextField.getText().equals(""))
+						Core.draw(formula, Double.parseDouble(multiplierTextField.getText()), drawCycle);
+					else
+						formulaField.setText("Insert a Multiplier into the text field to the left");
+
+				}
+
+			}
+
+		});
+
+		yLabel = new CLabel(shell, SWT.SHADOW_NONE);
+		yLabel.setBounds(300, 490, 30, 40);
+		yLabel.setText("y = ");
+
+		formulaField = new Text(shell, SWT.CENTER);
+		formulaField.setBounds(300, 500, 650, 20);
+		formulaField.setMessage("Insert Here the Formula to Graph");
+
+		formulaField.addKeyListener(new KeyListener(){
+
+			@Override
+			public void keyReleased(KeyEvent e){
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e){
+
+				if(e.keyCode == 13){
+
+					System.out.println(formula);
+
+					if(! multiplierTextField.getText().equals(""))
+						Core.draw(formula, Double.parseDouble(multiplierTextField.getText()), drawCycle);
+					else
+						formulaField.setText("Insert a Multiplier into the text field to the left");
+
+				}
+
+			}
+
+		});
+
+	}
+
+	private void initButtons(){
+
+		extendButton = new Button(shell, SWT.PUSH);
+		extendButton.setBounds(0, 10, 40, 25);
+		extendButton.setText("...");
+		extendButton.addMouseListener(new MouseAdapter(){
+
+			@Override
+			public void mouseDown(MouseEvent e){
+				System.out.println("...");
+				extended = ! extended;
+
+				if(extended){
+
+					multiplierTextField.dispose();
+					formulaField.dispose();
+
+					yLabel.dispose();
+					multLabel.dispose();
+
+					keyBoard = new KeyBoard(25, 300, shell);
+					keyBoard.initKeyBoard();
+
+					initStackLabels();
+
+					shell.layout();
+
+				}else{
+
+					keyBoard.dispose();
+
+					xStackLabel.dispose();
+					yStackLabel.dispose();
+					zStackLabel.dispose();
+					tStackLabel.dispose();
+
+					initTextField();
+
+				}
+			}
+
+		});
+
+	}
+
+	/*
+	 * Timer to update the Label and do some other cool stuff
+	 */
+
+	private void initTimer(){
+		
+		Runnable timer = new Runnable() {
+			public void run() {
+				display.timerExec(time, this);
+
+				if(!formulaField.isDisposed()){
+
+					if(!formula.equals(formulaField.getText())){
+						shell.redraw(); 
+						changed = true;
+						resetCicle();
+					}
+
+					formula = formulaField.getText();
+
+					changed = false;
+					
+					if(!extended)
+						expressionLabel.setText(formula);
+
+				}
+
+				if(extended && xStackLabel != null && yStackLabel != null && zStackLabel != null && tStackLabel != null){
+					xStackLabel.setText("X: " + Core.x);
+					yStackLabel.setText("Y: " + Core.y);
+					zStackLabel.setText("Z: " + Core.z);
+					tStackLabel.setText("T: " + Core.t);
+				}
+
+			}
+		};
+
+		display.timerExec(time, timer);
+
+	}
+
+	public static CLabel getLabel(){
+		return expressionLabel;
+	}
+
+	public static CLabel getXStackLabel(){
+		return xStackLabel;
+	}
+
+	public static CLabel getYStackLabel(){
+		return yStackLabel;
+	}
+
+	public static CLabel getZStackLabel(){
+		return zStackLabel;
+	}
+
+	public static CLabel getTStackLabel(){
+		return tStackLabel;
+	}
+
+	public static boolean isChanged(){
+		return changed;
+	}
+
+	public static void setChanged(boolean changed){
+		MainView.changed = changed;
+	}
+	
+	public static void augmentCicle(){
+		drawCycle++;
+	}
+	
+	public static void resetCicle(){
+		drawCycle = 0;
+	}
+	
+	/**
+	 * This ensures that the shell stays open until it's closed
+	 */
+
+	public void open(){
+		shell.open();
+		while(!shell.isDisposed()) {
+			if(!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Puts the shell at the center of the screen, just aesthetic
+	 */
+
+	private static void center(){
+		org.eclipse.swt.graphics.Rectangle bds = shell.getDisplay().getBounds();
+		org.eclipse.swt.graphics.Point p = shell.getSize();
+		int nLeft = (bds.width - p.x) / 2;
+		int nTop = (bds.height - p.y) / 2;
+		shell.setBounds(nLeft, nTop, p.x, p.y);
+	}
+
+}
